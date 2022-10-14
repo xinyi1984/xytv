fb0115e863a47a91418787e3da650438a8dd416b
xy <75725458@qq.com>
2022-10-14 03:10:53 +0000

    update py_yydshd.py.
    Signed-off-by: xy <75725458@qq.com>

--- py_yydshd.py
+++ py_yydshd.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 			name = vod['title']
 			pic = vod['cover']
 			sid = vod['id']
-            sid = vod['tag_id']
+            sid = tags['tag_id']
 			suffix = ''
 			if vod['is_vip'] == 1:
 				suffix = ' 会员'
