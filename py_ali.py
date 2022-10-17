18a55cec18f7f0b34df19c4fe48bb3cc4d5d6a63
xy <75725458@qq.com>
2022-10-17 02:05:39 +0000

    update py_ali.py.
    Signed-off-by: xy <75725458@qq.com>

--- py_ali.py
+++ py_ali.py
@@ -391,7 +391,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 		url = 'https://api.aliyundrive.com/token/refresh'
 		if len(self.authorization) == 0 or self.timeoutTick - self.localTime <= 600:
 			form = {
-				'refresh_token':'7a25b79483f240228eff5f968cafb934'
+				'refresh_token':'https://gitee.com/xinyitv/xinyi/raw/master/token.txt'
 			}
 			rsp = requests.post(url,json = form,headers=self.header)
 			jo = json.loads(rsp.text)
