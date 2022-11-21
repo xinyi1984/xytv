19c16e0cedd31c74beb55e3a2e28adcfb504a396
xinyi1984 <75725458@qq.com>
2022-11-21 08:36:47 +0000

    更新 'py_yiso.py'

--- py_yiso.py
+++ py_yiso.py
@@ -28,14 +28,14 @@ class Spider(Spider):
 		result = {}
 		return result
 	header = {
-		"User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36",
-		"Referer": "http://ali.freepan.net/"
+		"User-Agent": "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36",
+		"Referer": "https://yiso.fun/"
 	}
 	def detailContent(self,array):
 		return self.ali.detailContent(array)
 
 	def searchContent(self,key,quick):
-		url = "http://ali.freepan.net/?q={0}&from=ali".format(key)
+		url = "https://yiso.fun/api/search?name={0}&from=ali".format(key)
 		vodList = requests.get(url=url, headers=self.header, verify=False).json()["data"]["list"]
 		videos = []
 		for vod in vodList:
