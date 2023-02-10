6a27e1a2369576bed90b98ef01e8bae66b70cd82
xy <75725458@qq.com>
2023-02-10 04:52:20 +0000

    update py_yydshd.py.
    Signed-off-by: xy <75725458@qq.com>

--- py_yydshd.py
+++ py_yydshd.py
@@ -77,7 +77,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def getHeader(self,force=False):
 		if len(self.token) == 0 or force:
 			form  = {
-				'token':'96cb77372736a01ad6c8aefd7b2be5a4cb6f957d50f11d7f7529f6f173ebacfd'
+				'email': '399232408@qq.com',
+				'password': 'abcd1234'
 			}
 			if len(form['password']) > 0:
 				rsp = self.post("https://cmn.yydshd.com/api/login",data=form)
