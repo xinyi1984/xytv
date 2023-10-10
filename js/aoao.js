6acad4dfb57470e8784b352f7c4d6b92ac8e9634
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2023-10-10 10:33:58 +0800

    Update aoao.js

--- js/aoao.js
+++ js/aoao.js
@@ -19,27 +19,22 @@ var rule = {
                 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; NX669J Build/SKQ1.220502.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6055.0 Mobile Safari/537.36',
                 // "Cookie": "searchneed=ok"
             },
-    filter_def:{
-        duanju:{cateId:'duanju'},
-        dianying:{cateId:'dianying'},
-        dianshiju:{cateId:'dianshiju'},
-        zongyi:{cateId:'zongyi'},
-        dongman:{cateId:'dongman'}
-    },
-            class_parse: 'body&&.navbar&&ul&&li:gt(0);a&&Text;a&&href;.*/(.*?).html',
-            play_parse: true,
-            lazy: '',
-            limit: 6,
-            推荐: '.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-            double: true, // 推荐内容是否双层定位
-            一级: 'body a.module-poster-item.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-            二级: {
-                "title": "h1&&Text;.module-info-tag&&Text",
-                "img": ".lazyload&&data-original",
-                "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
-                "content": ".module-info-introduction&&Text",
-                "tabs": ".module-tab-item",
-                "lists": ".module-play-list:eq(#id) a"
-            },
+    class_name:'短剧&电影&电视剧&综艺&动漫', // 分类筛选 
+    class_url:'duanju&dianying&dianshiju&zongyi&dongman',
+    class_parse: 'body&&.navbar&&ul&&li:gt(0);a&&Text;a&&href;.*/(.*?).html',
+    play_parse: true,
+    lazy: '',
+    limit: 6,
+    推荐: '.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    double: true, // 推荐内容是否双层定位
+    一级: 'body a.module-poster-item.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    二级: {
+        "title": "h1&&Text;.module-info-tag&&Text",
+        "img": ".lazyload&&data-original",
+        "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
+        "content": ".module-info-introduction&&Text",
+        "tabs": ".module-tab-item",
+        "lists": ".module-play-list:eq(#id) a"
+        },
             搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
-        }
+    }
