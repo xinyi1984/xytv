a48b41e7bf621c922c56941625d395df65c84e04
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2024-04-23 19:03:31 +0800

    Update 短剧tv.js

--- js/短剧tv.js
+++ js/短剧tv.js
@@ -2,7 +2,7 @@ var rule ={
     title: '短剧网',
     host: 'http://www.duanjutv.cc',
     url: '/vodtype/fyclass-fypage.html',
-    searchUrl: '/vodsearch/**----------fypage---.html',
+    searchUrl: '/vodsearch/-------------.html?wd=**',
     searchable: 2,//是否启用全局搜索,
     quickSearch: 0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
@@ -14,9 +14,9 @@ var rule ={
     double: true, // 推荐内容是否双层定位
     一级: '.stui-pannel_bd;ul&&li;a&&Text;img&&src;a&&href',
     二级: {
-        "title": ".title&&Text;.detail-sketch&&Text",
-        "img": ".img&&img&&src",
-        "desc": ".stui-content__detail&&p:eq(1)&&Text.stui-content__detail&&p:eq(1)&&Text;.stui-content__detail&&p:eq(2)&&Text;.stui-content__detail&&p:eq(3)&&Text",
+        "title": "a&&title",
+        "img": "img&&src",
+        "desc": ".pic-text&&Text",
         "content": ".detail-sketch&&Text",
         "tabs": ".title&&Text",
         "lists": ".stui-content__playlist:eq(#id)&&li"
