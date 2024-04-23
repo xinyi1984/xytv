5cc62c6f759173b2db7b09f7c0aad7b109745557
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2024-04-23 18:03:06 +0800

    Update 短剧tv.js

--- js/短剧tv.js
+++ js/短剧tv.js
@@ -17,14 +17,14 @@ var rule = {
     lazy: '',
     推荐: '.stui-pannel_bd;ul&&li;a&&title;img&&src;a&&href',
     double: true,
-    一级: '.hl-vod-list&&.hl-list-item;a&&title;a&&data-original;.remarks&&Text;a&&href',
+    一级: '.stui-pannel_bd;ul&&li;a&&title;img&&src;a&&href',
     二级: {
-        "title": ".hl-infos-title&&Text;.hl-text-conch&&Text",
-        "img": ".hl-lazy&&data-original",
-        "desc": ".hl-infos-content&&.hl-text-conch&&Text",
-        "content": ".hl-content-text&&Text",
-        "tabs": ".hl-tabs&&a",
-        "lists": ".hl-plays-list:eq(#id)&&li"
+        "title": ".title&&Text;.detail-sketch&&Text",
+        "img": ".img&&img&&src",
+        "desc": ".stui-content__detail&&p:eq(1)&&Text.stui-content__detail&&p:eq(1)&&Text;.stui-content__detail&&p:eq(2)&&Text;.stui-content__detail&&p:eq(3)&&Text",
+        "content": ".detail-sketch&&Text",
+        "tabs": ".title&&Text",
+        "lists": ".stui-content__playlist:eq(#id)&&li"
         },
     搜索: '.hl-list-item;a&&title;a&&data-original;.remarks&&Text;a&&href',
             searchable: 2,//是否启用全局搜索,
