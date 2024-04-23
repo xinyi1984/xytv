62c1db08b25d9a95c554310ffbe7a7a069f24c54
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2024-04-23 20:24:20 +0800

    Update 短剧tv.js

--- js/短剧tv.js
+++ js/短剧tv.js
@@ -10,12 +10,12 @@ var rule ={
     play_parse: true,
     lazy: '',
     limit: 6,
-    推荐: 'body&&.stui-pannel_bd;a&&title;img&&src;a&&href',
+    推荐: '.stui-pannel_bd;ul&&li;a&&title;img&&src;a&&href',
     double: true, // 推荐内容是否双层定位
     一级: 'body&&.stui-pannel_bd;a&&title;img&&src;a&&href',
     二级: {
         "title": "a&&title",
-        "img": "img&&src",
+        "img": "img",
         "desc": ".pic-text&&Text",
         "content": ".detail-sketch&&Text",
         "tabs": ".title&&Text",
