3ddb2b3633c56a7bf43bf7646b5800a18a28f703
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2024-04-24 11:50:31 +0800

    Update 短剧网.js

--- js/短剧网.js
+++ js/短剧网.js
@@ -7,13 +7,13 @@ var rule ={
     quickSearch: 0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
 	headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-		'User-Agent':'MOBILE_UA'
+		'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
 	},
     class_parse: '.nav&&ul&&li;a&&Text;a&&href;.*/(.*?).html',
     play_parse: true,
     lazy: '',
     limit: 6,
-    推荐: '.module-items;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+    推荐: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
     double: true, // 推荐内容是否双层定位
     一级: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
     二级: {
