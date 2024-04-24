ae65db5607c32fb80e96c157bb2405c7a0ca3a57
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2024-04-24 10:18:02 +0800

    Update 短剧网.js

--- js/短剧网.js
+++ js/短剧网.js
@@ -2,7 +2,7 @@ var rule ={
     title: '短剧网',
     host: 'http://5uduanju.com',
     url: '/vodtype/fyclass-fypage.html',
-    searchUrl: '/vodsearch/-------------.html?wd=**&submit=',
+    searchUrl: '//vodsearch/**----------fypage---.html',
     searchable: 2,//是否启用全局搜索,
     quickSearch: 0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
@@ -16,7 +16,7 @@ var rule ={
     二级: {
         "title": "h1&&Text;.module-item-text&&Text",
         "img": ".module-item-pic&&img&&data-src",
-        "desc": ".video-info-items:eq(3)&&Text;.video-info-items:eq(4)&&Text;.video-info-items:eq(5)&&Text;",
+        "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;",
         "content": ".vod_content&&Text",
         "tabs": ".module-tab-item",
         "lists": ".module-player-list:eq(#id)&&.scroll-content&&a"
